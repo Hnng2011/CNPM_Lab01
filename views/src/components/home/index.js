8131c7867f00f5b6d153e693bb5cfd74870ab2ee
Hnng2011 <105967867+Hnng2011@users.noreply.github.com>
2022-06-24 12:05:58 +0700

    Enter

--- views/src/components/home/index.js
+++ views/src/components/home/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import axios from 'axios';
 import './index.css';
 
+import Navbar from './section/navbar';
 
 
 
---
+++ views/src/components/home/section/navbar.js
@@ -0,0 +1,21 @@
+export default function Navbar() {
+    return (
+        <body id="Border">
+            <nav id="container">
+                <img id="image"></img>
+                <li id="item">
+                    <button class="coolBeans">
+                        Sign In
+                    </button>
+                    <button class="coolBeans">
+                        Register
+                    </button>
+                </li>
+
+            </nav>
+        </body>
+    );
+}
+
+
+
